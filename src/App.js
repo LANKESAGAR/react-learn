5537e27c824d2fb06079856a02fb7e97486b15fa
sagar <sagarl.edu2022@gmail.com>
2025-03-08 11:32:36 -0500

    props

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Employee from './components/Employee';
 function App() {
   console.log("We are about to list the employees");
   const showEmployees = true;
-  
+
   return (
     <div className="App">
       {console.log(`Inside the return, showEmployees ${showEmployees}`)}
       {showEmployees ?
-        <><Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee /></>
+        <>
+          <Employee name="sagar" role="Engineer" />
+          <Employee name="john" role="Doctor" />
+          <Employee name="roni" role="Lawyer" />
+          <Employee name="patty" role="police" />
+          <Employee name="Mandy" />
+        </>
         : <p>You are not allowed to see the employees</p>
       }
     </div>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,11 @@
-function Employee(){
-    return <h3>Here is an employee</h3>
+function Employee(props) {
+    return (
+        <>
+            <h3>Employee {props.name}</h3>
+            <p>{props.role ? props.role : "No Role Defined"}</p>
+            {props.role ? <p className="role">{props.role}</p> : <p className="norole">No Role Defined</p>}
+        </>
+    );
 }
 
 export default Employee;
