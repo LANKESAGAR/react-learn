0518e6548edcbe141c61d78a3ac9fee044dab323
sagar <sagarl.edu2022@gmail.com>
2025-03-08 14:39:26 -0500

    Install Tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
 
@@ -7,7 +7,7 @@ function App() {
   console.log("We are about to list the employees");
   const showEmployees = true;
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       {console.log(`Inside the return, showEmployees ${showEmployees}`)}
       {showEmployees ?
         <>
