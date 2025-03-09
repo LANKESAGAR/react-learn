f38ad49025c1ee826d42af1a1aa4fdb2617f76dd
sagar <sagarl.edu2022@gmail.com>
2025-03-09 14:31:18 -0400

    Header

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,12 +1,12 @@
-import './index.css';
-import Employee from './components/Employee';
+import '../index.css';
+import Employee from '../components/Employee';
 import { useState } from 'react';
 import {v4 as uuid4} from 'uuid';
-import AddEmployee from './components/AddEmployee copy';
-import EditEmployee from './components/EditEmployee';
+import AddEmployee from '../components/AddEmployee copy';
+import EditEmployee from '../components/EditEmployee';
 import Header from './components/Header';
 
-function App() {
+function Employees() {
   const [role, setRole] = useState('role');
   const [employees, setEmployees] = useState([
     {
@@ -105,4 +105,4 @@ function App() {
   );
 }
 
-export default App;
+export default Employees;
