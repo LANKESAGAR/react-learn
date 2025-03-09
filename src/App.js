08b210a38596c8f764ff6973b39489db066269d6
sagar <sagarl.edu2022@gmail.com>
2025-03-09 15:24:41 -0400

    Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './index.css';
 import Header from './components/Header';
 import Employees from './pages/Employees';
+import Customers from './pages/Customer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Header>
-      <Employees />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/employees' element={<Employees />} />
+          <Route path='/customers' element={<Customers />} />
+        </Routes>
+      </BrowserRouter>
     </Header>
 
-      );
+  );
 }
 
-      export default App;
+export default App;
