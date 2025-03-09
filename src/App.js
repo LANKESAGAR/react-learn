824aa5a99d13e434eb05642132f4a3229d9fed25
sagar <sagarl.edu2022@gmail.com>
2025-03-09 16:48:22 -0400

    Effect Practice

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Employees from './pages/Employees';
 import Customers from './pages/Customer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dictionary from './components/Dictionary';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/employees' element={<Employees />} />
           <Route path='/customers' element={<Customers />} />
+          <Route path='/dictionary' element={<Dictionary />} />
         </Routes>
       </Header>
     </BrowserRouter>
