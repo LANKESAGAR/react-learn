7ec2b42a3b9276f26089cd44872acba0a291ef7d
sagar <sagarl.edu2022@gmail.com>
2025-03-13 19:46:29 -0400

    Renamed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './index.css';
 import Header from './components/Header';
 import Employees from './pages/Employees';
-import Customers from './pages/Customer';
+import Customers from './pages/Customers';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dictionary from './pages/Dictionary';
 import Definition from './pages/Definition';
