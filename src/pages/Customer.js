bba820449a31897d53c1e8633a7b0757e98828ac
sagar <sagarl.edu2022@gmail.com>
2025-03-12 21:10:25 -0400

    Fetch Data from Backend

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,3 +1,24 @@
-export default function Customers(){
-    return "Customers";
+import { useEffect, useState } from "react";
+
+export default function Customers() {
+    const [customers, setCustomers] = useState();
+    useEffect(() => {
+        console.log('Fetching...')
+        fetch('http://localhost:8000/api/customers/')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setCustomers(data.customers);
+            });
+    }, []);
+    return (
+        <>
+            <h1>Here are our customers</h1>
+            {customers ?  customers.map((customer)=>{
+                return <p>{customer.name}</p>;
+            }) : ''
+        }
+        </>
+
+    );
 }
