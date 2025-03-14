d5a49013f4d4083fd5cf0ef76f06db733f0eef4a
sagar <sagarl.edu2022@gmail.com>
2025-03-13 20:45:42 -0400

    base url for customers.js

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
+import { baseurl } from "../shared";
 
 export default function Customers() {
     const [customers, setCustomers] = useState();
     useEffect(() => {
         console.log('Fetching...')
-        fetch('http://localhost:8000/api/customers/')
+        const url = baseurl + 'api/customers/';
+        fetch(url)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data);
                 setCustomers(data.customers);
             });
     }, []);
