1b2c25ebe73e08f4f0eda2107b8e12050d9dd1a1
sagar <sagarl.edu2022@gmail.com>
2025-03-15 14:27:14 -0400

    Cancel button

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,52 +1,77 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Link, useNavigate} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import NotFound from "../components/NotFound";
 import { baseurl } from "../shared";
 
 export default function Customer() {
     const { id } = useParams();
     const [customer, setCustomer] = useState(null);
+    const [tempCustomer, setTempCustomer] = useState();
     const [notFound, setNotFound] = useState();
     const navigate = useNavigate();
+    const [changed, setChanged] = useState(false);
+
     useEffect(() => {
-        if (!id) return; 
+        console.log('customer', customer);
+        console.log('tempCustomer', tempCustomer);
+        console.log('changed', changed)
+    });
+
+    useEffect(() => {
+        if (!id) return;
         const url = baseurl + 'api/customer/' + id;
         fetch(url)
             .then((response) => {
-                if(response.status === 404){
+                if (response.status === 404) {
                     setNotFound(true);
                 }
                 return response.json()
             })
             .then((data) => {
                 setCustomer(data.customer);
+                setTempCustomer(data.customer);
             })
             .catch((error) => console.error("Error fetching customer:", error));
-    }, [id]); 
+    }, [id]);
 
     return (
         <>
-        {notFound ? <p>The customer with id {id} is not found</p> : null}
+            {notFound ? <p>The customer with id {id} is not found</p> : null}
             {customer ? <div>
-                <p>{customer.id}</p>
-                <p>{customer.name}</p>
-                <p>{customer.industry}</p>
+                <input className='m-2 block px-2' type='text' value={tempCustomer.name}
+                    onChange={(e) => {
+                        setChanged(true);
+                        setTempCustomer({ ...tempCustomer, name: e.target.value })
+                    }} />
+                <input className='m-2 block px-2' type='text' value={tempCustomer.industry}
+                    onChange={(e) => {
+                        setChanged(true);
+                        setTempCustomer({ ...tempCustomer, industry: e.target.value })
+                    }} />
+                {changed ? <>
+                    <button onClick={(e) =>{
+                        setTempCustomer({...customer});
+                        setChanged(false);
+                    } }>Cancel</button> <button>Save</button>
+                </> : null}
             </div> : null}
-            <button onClick={(e)=>{
+            <button onClick={(e) => {
                 const url = baseurl + '/api/customer/' + id;
-                fetch(url, {method: 'DELETE', headers:{
-                    'Content-Type': 'application/json'
-                }})
-                .then((response)=>{
-                    if(!response.ok){
-                        throw new Error('Something went wrong')
+                fetch(url, {
+                    method: 'DELETE', headers: {
+                        'Content-Type': 'application/json'
                     }
-                    navigate('/customers');
                 })
-                .catch((e)=>{
-                    console.log(e);
-                })
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error('Something went wrong')
+                        }
+                        navigate('/customers');
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                    })
             }}>DELETE</button>
             <br />
             <Link to='/customers'>Go Back</Link>
