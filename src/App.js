2b2449cb0f6a5763cf83e4aab7a940372ebe4ae5
sagar <sagarl.edu2022@gmail.com>
2025-03-18 20:53:59 -0400

    Refresh Tokens

--- src/App.js
+++ src/App.js
@@ -8,11 +8,39 @@ import Definition from './pages/Definition';
 import NotFound from './components/NotFound';
 import Customer from './pages/Customer';
 import Login from './pages/Login';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import { baseurl } from './shared';
 
 export const LoginContext = createContext();
 
 function App() {
+  useEffect(()=>{
+    function refreshTokens(){
+      if(localStorage.refresh){
+        const url = baseurl + 'api/token/refresh/';
+        fetch(url,{
+          method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                refresh: localStorage.refresh
+              }),
+        })
+        .then((response)=>{
+          return response.json();
+        })
+        .then((data)=>{
+          localStorage.access = data.access;
+          localStorage.refresh = data.refresh;
+          setLoggedIn(true);
+        })
+      }
+    }
+    const min = 1000*60
+    refreshTokens();
+;    setInterval(refreshTokens,min*3)
+  })
   const [loggedIn, setLoggedIn] = useState(true);
 
   function changeLoggedIn(value){
